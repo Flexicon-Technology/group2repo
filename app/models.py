0d84517c538c39e834ed71a2f30e97d2f0c907b1
David Nwabuzor <davidpainter421@gmail.com>
2024-06-04 11:10:08 +0100

    created a user creation model

--- app/models.py
+++ app/models.py
@@ -1,9 +1,30 @@
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+  __tablename__ = 'users'
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(80), unique=True, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+
+  #password hashing!
+  password_hash = db.Column(db.String(80), nullable=False)
+
+  @property
+  def password(self):
+    raise AttributeError('password is not a readable Attribute!')
+
+  @password.setter
+  def password(self, password):
+    self.password_hash = generate_password_hash(password)
+
+  def verify_password(self, password):
+    return check_password_hash(self.password_hash, password)
+
+  def __init__(self, username, email, password):
+    self.username = username
+    self.email = email
+    self.password = password
 
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
